391d7944cc2ada560c22970685b1f5bc6555f24e
MatheusVedovelli <matheusvedovelli@hotmail.com>
2020-05-14 09:44:06 +0900

    Project's Initial Commit

---
+++ www/js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+  $("#btn1").click(function(){
+    navigator.notification.alert("Alerta");//
+  });
+  $("#btn2").click(function(){
+    navigator.notification.confirm("Alerta", () => {}, "Confirm", ['a', 'b']);
+  });
+  $("#btn3").click(function(){
+    console.log("Teste");
+    navigator.notification.beep(3);
+  });
+  $("#btn4").click(function(){
+    navigator.vibrate(6000);
+  });
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    L.mapquest.key = 'BPYBTKxImVS6W97iL4Xy0TMQPhZ0aI7x';
+    let map = L.mapquest.map('map', {
+      center: [position.coords.latitude, position.coords.longitude],//[37.7749, -122.4194],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 17
+    });
+
+    map.addControl(L.mapquest.control());
+    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+  });
+
+});
